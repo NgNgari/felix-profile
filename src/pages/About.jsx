69d7bb89816956838d8578b38e5c390a68dc3bc0
Felix Ngari <Felix.Ngari@standrews.local>
2026-08-27 07:34:25 +0300

    Build responsive design foundation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,15 @@
 function About() {
   return (
-    <main>
-      <h1>About</h1>
-      <p>Felix Ngari</p>
+    <main className="container">
+      <h1>Felix Ngari</h1>
+
+      <p>
+        Business Development × Technology × Education
+      </p>
+
+      <p>
+        This is the foundation of my personal profile.
+      </p>
     </main>
   );
 }
